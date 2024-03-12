01272da95ad78097ee66c10c94f6733fafec679a
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-12 17:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+function addListItem() {
+  // Get the list element
+  var list = document.getElementById('list');
 
+  // Create a new list item
+  var newItem = document.createElement('li');
+
+  // Add some text to the list item
+  newItem.textContent = 'New item';
+
+  // Append the new list item to the list
+  list.appendChild(newItem);
+}
+
+// Add 10 list items when the page loads
+for (var i = 0; i < 10; i++) {
+  addListItem();
+}
+window.addEventListener('scroll', function() {
+  // If the user has scrolled to the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    // Add two new list items
+    addListItem();
+    addListItem();
+  }
+});
